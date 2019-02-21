13117ff8cd181bbad63a5565309cb597da71bdf8
malcom <me@malcom.pl>
2019-02-21 22:11:34 +0100

    Show seller info on auction summary page too

--- allegro-seller-info.user.js
+++ allegro-seller-info.user.js
@@ -2,7 +2,7 @@
 // @name         Allegro Seller Info
 // @namespace    http://malcom.pl/
 // @version      0.0.9
-// @description  Show the seller's name and location on the offers lists
+// @description  Show the seller's name and location on the offers lists and summary pages
 // @author       Malcom <me@malcom.pl>
 // @match        allegro.pl/*
 // @grant        none
@@ -11,67 +11,123 @@
 (function () {
 	'use strict';
 
-	if (!window.__listing_StoreState_base)
-		return;
+	// strona z lista aukcji
+	if (window.__listing_StoreState_base) {
 
-	const cssName = 'mX6tzke5';
-	document.body.insertAdjacentHTML('beforeend', `
-		<style type="text/css">
-			div.${cssName} { position: absolute; top: 0; right: 0; }
-			div.${cssName} > span { display: block; text-align: right; }
-			div.${cssName} > span.seller a { color: #00a790; text-decoration: none; }
-			div.${cssName} > span.seller a: link { color: #00a790; }
-			div.${cssName} > span.seller a: visited { color: #006456; }
-			div.${cssName} > span.location { margin-bottom: 10px; }
-		</style>
-	`);
+		const cssName = 'mX6tzke5';
+		document.body.insertAdjacentHTML('beforeend', `
+			<style type="text/css">
+				div.${cssName} { position: absolute; top: 0; right: 0; }
+				div.${cssName} > span { display: block; text-align: right; }
+				div.${cssName} > span.seller a { color: #00a790; text-decoration: none; }
+				div.${cssName} > span.seller a: link { color: #00a790; }
+				div.${cssName} > span.seller a: visited { color: #006456; }
+				div.${cssName} > span.location { margin-bottom: 10px; }
+			</style>
+		`);
 
-	const listNode = document.getElementsByClassName('opbox-listing--base')[0].firstElementChild;
+		const listNode = document.getElementsByClassName('opbox-listing--base')[0].firstElementChild;
 
-	function UpdateList() {
+		function UpdateList() {
 
-		for (var node of listNode.getElementsByTagName('article')) {
+			for (var node of listNode.getElementsByTagName('article')) {
 
-			var item;
-			for (var i in node) {
-				if (i.startsWith('__reactEventHandlers')) {
-					item = node[i].children.props.item;
-					break;
+				var item;
+				for (var i in node) {
+					if (i.startsWith('__reactEventHandlers')) {
+						item = node[i].children.props.item;
+						break;
+					}
 				}
+				if (!item) return;
+
+				node = node.children[0].children[0].children[1].children[0];
+				node.style.position = 'relative';
+
+				node.insertAdjacentHTML('beforeend', `
+					<div class="${cssName}">
+						<span class="seller"><a href="${item.seller.userListingUrl}">${item.seller.login}</a></span>
+						<span class="location">${item.location}</span>
+					</div>
+				`);
+
+				// jesli jest logo sklepu to przenies je do naszego kontenera,
+				// zeby wyswietlalo sie pod nasza wstawka...
+				if (node.firstElementChild.nodeName != 'H2')
+					node.lastElementChild.appendChild(node.firstElementChild);
+
 			}
-			if (!item) return;
-
-			node = node.children[0].children[0].children[1].children[0];
-			node.style.position = 'relative';
-
-			node.insertAdjacentHTML('beforeend', `
-				<div class="${cssName}">
-					<span class="seller"><a href="${item.seller.userListingUrl}">${item.seller.login}</a></span>
-					<span class="location">${item.location}</span>
-				</div>
-			`);
-
-			// jesli jest logo sklepu to przenies je do naszego kontenera,
-			// zeby wyswietlalo sie pod nasza wstawka...
-			if (node.firstElementChild.nodeName != 'H2')
-				node.lastElementChild.appendChild(node.firstElementChild);
-
 		}
+
+		// na czas aktualizacji/odswiezania listy, element 'listNode'
+		// posiada dodatkowa klase stylow, wiec jej usuniecie jest
+		// rownoznaczne z zakonczeniem operacji przebudowania listy
+		// class: '_1e83564' -> '_1e83564 d101523' ->  '_1e83564'
+
+		var mutationObserver = new MutationObserver(function (mutations) {
+			mutations.forEach(function (m) {
+				if (m.attributeName == 'class' && m.target.className.indexOf(' ') == -1)
+					UpdateList();
+			});
+		});
+		mutationObserver.observe(listNode, { attributes: true });
+
+		UpdateList();
+		return;
 	}
 
-	// na czas aktualizacji/odswiezania listy, element 'listNode'
-	// posiada dodatkowa klase stylow, wiec jej usuniecie jest
-	// rownoznaczne z zakonczeniem operacji przebudowania listy
-	// class: '_1e83564' -> '_1e83564 d101523' ->  '_1e83564'
+	// strona z opisem aukcji
+	if (window.__PROPS__ALLEGRO_SHOWOFFER_SUMMARY__) {
 
-	var mutationObserver = new MutationObserver(function (mutations) {
-		mutations.forEach(function (m) {
-			if (m.attributeName == 'class' && m.target.className.indexOf(' ') == -1)
-				UpdateList();
-		});
-	});
-	mutationObserver.observe(listNode, { attributes: true });
+		const cssName = 'm8dkm36f';
+		document.body.insertAdjacentHTML('beforeend', `
+			<style type="text/css">
+				div.${cssName} {
+					font-size: 13px;
+					color: #767676;
+					margin-bottom: 8px;
+					line-height: 21px;
+				}
+			</style>
+		`);
 
-	UpdateList();
+		// pobierz lokalizacje z Opcji dostawy
+		var node = document.getElementsByName('delivery')[0].nextElementSibling;
+
+		var loc;
+		for (var p of node.getElementsByTagName('p')) {
+			if (p.innerText.startsWith('Lokalizacja:')) {
+				var i = p.innerText.lastIndexOf(', Polska');
+				loc = i != -1 ? p.innerText.substr(0, i) : p.innerText;
+				break;
+			}
+		}
+		if (!loc) return;
+
+		// wstaw lokalizacje w naglowku z info aukcji
+		node = document.querySelector('div[itemprop="offers"]');
+
+		// zaleznie od aukcji i jej stanu/konfiguracji, rozne komorki w layoucie
+		// sa wypenione roznymi danymi, a ilosc wierszy i dzieci moze byc rozna,
+		// dlatego szukam pierwszego dziecka z border i lapiemy jego poprzednika
+		node = [...node.children].filter(n => {
+			return n.computedStyleMap().get('border-top-width').value == 1;
+		})[0].previousElementSibling;
+
+		// gdy wiersz jest pelny, dodajemy nowy
+		if (node.childElementCount > 1) {
+			node.insertAdjacentHTML('afterend', `<div class="${node.className}"></div>`);
+			node = node.nextElementSibling;
+		}
+
+		// gdy wiersz pusty dodajemy pusty kontener jako pierwsza komorka
+		if (node.childElementCount == 0) {
+			node.insertAdjacentHTML('beforeend', `<div></div>`);
+		}
+
+		// a jako druga komorka leci nasza wstawka
+		node.insertAdjacentHTML('beforeend', `<div class="${cssName}">${loc}</div>`);
+		return;
+	}
 
 })();
