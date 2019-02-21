c768cbdcc7bd1caddc478721b98613b701101de1
malcom <me@malcom.pl>
2019-02-22 00:22:20 +0100

    Workaround for 'Background Tab Resource Load Throttling' issue on Allego pages

--- allegro-seller-info.user.js
+++ allegro-seller-info.user.js
@@ -91,42 +91,57 @@
 			</style>
 		`);
 
-		// pobierz lokalizacje z Opcji dostawy
-		var node = document.getElementsByName('delivery')[0].nextElementSibling;
+		function UpdateOffer() {
 
-		var loc;
-		for (var p of node.getElementsByTagName('p')) {
-			if (p.innerText.startsWith('Lokalizacja:')) {
-				var i = p.innerText.lastIndexOf(', Polska');
-				loc = i != -1 ? p.innerText.substr(0, i) : p.innerText;
-				break;
+			// pobierz lokalizacje z Opcji dostawy
+			var node = document.getElementsByName('delivery')[0].nextElementSibling;
+
+			var loc;
+			for (var p of node.getElementsByTagName('p')) {
+				if (p.innerText.startsWith('Lokalizacja:')) {
+					var i = p.innerText.lastIndexOf(', Polska');
+					loc = i != -1 ? p.innerText.substr(0, i) : p.innerText;
+					break;
+				}
 			}
+			if (!loc) return;
+
+			// wstaw lokalizacje w naglowku z info aukcji
+			node = document.querySelector('div[itemprop="offers"]');
+
+			// zaleznie od aukcji i jej stanu/konfiguracji, rozne komorki w layoucie
+			// sa wypenione roznymi danymi, a ilosc wierszy i dzieci moze byc rozna,
+			// dlatego szukam pierwszego dziecka z border i lapiemy jego poprzednika
+			node = [...node.children].filter(n => {
+				return n.computedStyleMap().get('border-top-width').value == 1;
+			})[0].previousElementSibling;
+
+			// gdy wiersz jest pelny, dodajemy nowy
+			if (node.childElementCount > 1) {
+				node.insertAdjacentHTML('afterend', `<div class="${node.className}"></div>`);
+				node = node.nextElementSibling;
+			}
+
+			// gdy wiersz pusty dodajemy pusty kontener jako pierwsza komorka
+			if (node.childElementCount == 0) {
+				node.insertAdjacentHTML('beforeend', `<div></div>`);
+			}
+
+			// a jako druga komorka leci nasza wstawka
+			node.insertAdjacentHTML('beforeend', `<div class="${cssName}">${loc}</div>`);
 		}
-		if (!loc) return;
 
-		// wstaw lokalizacje w naglowku z info aukcji
-		node = document.querySelector('div[itemprop="offers"]');
+		// Workaround/Hack
+		// Background Tab Resource Load Throttling ma jakis problem na Allegro,
+		// bo pomimo dodania wstawki ona znika z DOM-a po wyjsciu zakladki z tla, wiec
+		// jesli po zmianie widocznosci strony naszej wstaki nie ma to dodajemy ponownie
 
-		// zaleznie od aukcji i jej stanu/konfiguracji, rozne komorki w layoucie
-		// sa wypenione roznymi danymi, a ilosc wierszy i dzieci moze byc rozna,
-		// dlatego szukam pierwszego dziecka z border i lapiemy jego poprzednika
-		node = [...node.children].filter(n => {
-			return n.computedStyleMap().get('border-top-width').value == 1;
-		})[0].previousElementSibling;
+		document.addEventListener('visibilitychange', function () {
+			if (!document.hidden && document.getElementsByClassName(cssName).length == 0)
+				UpdateOffer();
+		});
 
-		// gdy wiersz jest pelny, dodajemy nowy
-		if (node.childElementCount > 1) {
-			node.insertAdjacentHTML('afterend', `<div class="${node.className}"></div>`);
-			node = node.nextElementSibling;
-		}
-
-		// gdy wiersz pusty dodajemy pusty kontener jako pierwsza komorka
-		if (node.childElementCount == 0) {
-			node.insertAdjacentHTML('beforeend', `<div></div>`);
-		}
-
-		// a jako druga komorka leci nasza wstawka
-		node.insertAdjacentHTML('beforeend', `<div class="${cssName}">${loc}</div>`);
+		UpdateOffer();
 		return;
 	}
 
